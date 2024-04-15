9646200b4ac9788a7615935f6ba6559322d681ca
hasan-tec <131611130+hasan-tec@users.noreply.github.com>
2024-04-15 23:56:40 +0500

    wtf

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignIn from '../src/components/Auth/SignIn';
 import SignUp from '../src/components/Auth/SignUp';
 import HomePage from '../src/components/Auth/HomePage';
-import Dalle from '../src/components/Dalle/Dalle';
 
 
 const App = () => {
@@ -14,7 +13,7 @@ const App = () => {
         <Route exact path="/signin" element={<SignIn />} />
         <Route exact path="/signup" element={<SignUp />} />
         <Route exact path="/HomePage" element={<HomePage />} />
-        <Route exact path="/Dalle" element={<Dalle />} />
+        
       </Routes>
     </Router>
   );
